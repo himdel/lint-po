df154b819207e33f612fb5c931475ceaf44bb064
Martin Hradil <mhradil@redhat.com>
2021-12-03 22:23:02 +0000

    setup.py: add README, url

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
   name="lint-po",
   description='A simple gettext .po linter to check for mangled variable names in translations',
-  version="0.1.0",
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  version="0.1.1",
   license='Apache License 2.0',
   py_modules=["lint_po"],
   scripts=["bin/lint-po"],
+  url='https://github.com/himdel/lint-po',
 )
